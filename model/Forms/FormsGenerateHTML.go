f27c8a349788f67ea3f7aa17486272b5a1bfdb0f
Chris Guthrey <chrisg@hallensteins.com>
2023-05-17 13:40:46 +1200

    fix the html generation for radio buttons

--- model/Forms/FormsGenerateHTML.go
+++ model/Forms/FormsGenerateHTML.go
@@ -15,12 +15,96 @@ func GenerateHTML(appCtx *appContextConfig.Application, formName string) (string
 	}
 
 	html := ""
-	currentRadioButtonFieldName := ""
-	currentRadioButtonContainer := ""
-	currentRadioButtonChildren := ""
-
+	state := "start"
+	radioName := ""
 	for _, q := range questions {
-		appCtx.LogInfo(fmt.Sprint(q.InputFieldName, ", ", q.QuestionType, ", ", q.TemplateName))
+
+		appCtx.LogInfo(fmt.Sprint("InputFieldName: ", q.InputFieldName, ", QuestionType: ", q.QuestionType, ", TemplateName: ", q.TemplateName, ", State: ", state))
+		if state == "start" {
+			if q.TemplateName == "StdText" || q.TemplateName == "StdTextArea" {
+				state = "std"
+				html += stdField(q)
+			} else if q.TemplateName == "RadioButton" {
+				state = "radio"
+				radioName = q.InputFieldName
+				html += startRadio(q)
+			}
+		} else if state == "std" {
+			if q.TemplateName == "RadioButton" {
+				state = "radio"
+				radioName = q.InputFieldName
+				html += startRadio(q)
+			} else {
+				html += stdField(q)
+			}
+		} else if state == "radio" {
+			if q.TemplateName == "RadioButton" { //continue with radio
+				if radioName != q.InputFieldName {
+					html += finishRadio()
+					radioName = q.InputFieldName
+					html += startRadio(q)
+				} else {
+					html += continueRadio(q)
+				}
+			} else {
+				html += finishRadio()
+				state = "std"
+				html += stdField(q)
+			}
+		}
+	} //end for range loop
+	if state == "radio" {
+		html += finishRadio()
+	}
+
+	return html, nil
+} //end func
+
+func startRadio(q WebFormQuestion) string {
+	rpl := strings.Replace(q.Template2, "{{ .InputFieldName }}", q.QuestionText, -1)
+	rpl += continueRadio(q)
+	return rpl
+}
+
+func continueRadio(q WebFormQuestion) string {
+	radioBtn := q.Template3
+	radioBtn = strings.Replace(radioBtn, "{{ .radioButtonName }}", q.InputFieldName, -1)
+	radioBtn = strings.Replace(radioBtn, "{{ .radioButtonID }}", q.InputFieldName, -1)
+	radioBtn = strings.Replace(radioBtn, "{{ .radioButtonID }}", q.InputFieldName, -1)
+	radioBtn = strings.Replace(radioBtn, "{{ .radioButtonValue }}", q.RadioOption, -1)
+	return radioBtn
+}
+
+func finishRadio() string {
+	rpl := `		</div>
+    </div>
+</div>`
+	return rpl
+}
+
+func stdField(q WebFormQuestion) string {
+	rpl := strings.Replace(q.Template1, "{{ .questionType }}", q.QuestionType, -1)
+	rpl = strings.Replace(rpl, "{{ .InputFieldName }}", q.InputFieldName, -1)
+	rpl = strings.Replace(rpl, "{{ .questionID }}", q.InputFieldName, -1)
+	rpl = strings.Replace(rpl, "{{ .questionID }}", q.InputFieldName, -1)
+	rpl = strings.Replace(rpl, "{{ .questionValue }}", "", -1)
+	if q.AnswerRequired == "Y" {
+		rpl = strings.Replace(rpl, "{{ .required }}", "required", -1)
+	} else {
+		rpl = strings.Replace(rpl, "{{ .required }}", "", -1)
+	}
+	rpl = strings.Replace(rpl, "{{ .questionLabel }}", q.QuestionText, -1)
+	return rpl
+}
+
+/*
+
+
+		currentRadioButtonFieldName := ""
+		currentRadioButtonContainer := ""
+		currentRadioButtonChildren := ""
+
+
 		rpl := ""
 
 		//are we in the middle of a radio button?
@@ -65,3 +149,4 @@ func GenerateHTML(appCtx *appContextConfig.Application, formName string) (string
 	}
 	return html, nil
 }
+*/
